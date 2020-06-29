2a06a7af5eaedf0d29a5b4957edbfd12193338b1
stnolting <stnolting@gmail.com>
2020-06-29 19:57:10 +0200

    minor fixes

--- .ci/test.sh
+++ .ci/test.sh
@@ -6,17 +6,19 @@ set -e
 # Just TEST the available toolchain versions
 TOOLCHAIN=riscv32-unknown-elf
 
-
 # Decompress and test
+echo ""
+
 TEST=riscv32-unknown-elf.gcc-9.2.0.rv32i.ilp32
-echo "" & echo "Testing $TEST/$TOOLCHAIN..."
-rm -rf $TEST && mkdir $TEST.tmp
+echo "Testing $TEST/$TOOLCHAIN..."
+rm -rf $TEST.tmp && mkdir $TEST.tmp
 tar -xzf data/$TEST.tar.gz -C $TEST.tmp/
 ./$TEST.tmp/bin/$TOOLCHAIN-gcc -v
+
+echo ""
 
 TEST=riscv32-unknown-elf.gcc-9.2.0.rv32e.ilp32e
-echo "" & echo "Testing $TEST/$TOOLCHAIN..."
-rm -rf $TEST && mkdir $TEST.tmp
+echo "Testing $TEST/$TOOLCHAIN..."
+rm -rf $TEST.tmp && mkdir $TEST.tmp
 tar -xzf data/$TEST.tar.gz -C $TEST.tmp/
 ./$TEST.tmp/bin/$TOOLCHAIN-gcc -v
-
